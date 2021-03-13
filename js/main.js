db474910a81f7e1195a56c5dc021285495528966
HonduranCoder <stef45689gmail.com>
2021-03-13 12:51:31 -0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//CODEWARS
+//Multiply 8kyu
+function multiply(a, b){
+  a * b
+}
+function multiply(a, b){
+  return a * b
+}
+
+multiply(4,5);
+
+//removeChar
+//8kyu
+function removeChar(str){
+  return str.slice(1,-1)
+}
+
+//Keep Hydrated 8kyu
+
+function litres(time) {
+  return Math.floor(time/2);
+}
+
+//8kyu
+function removeChar(str){
+  return str.slice(1,-1)
+}
+
+//String Ends with? 7kyu
+function solution(str, ending){
+ return str.indexOf(ending, str.length - ending.length) !== -1;
+}
+
+function sumStr(a,b) {
+return ((+a) + (+b)).toString();
+
+}
