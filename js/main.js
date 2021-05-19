caec9b8578abadbca2e03435dea4be62aeea0da6
HonduranCoder <stef45689@gmail.com>
2021-05-19 08:51:48 -0500

    More

--- js/main.js
+++ js/main.js
@@ -294,4 +294,5 @@ function patter(n){
 
 //Find the perfect square
 function findNextSquare(sq) {
- var sqRt = Math.sqrt( sq );
+ var sqRt = Math.sqrt( sq )
+ if ( NaN === sqRt || sqRt !== parseInt( sqRt, 10 ) ) {
