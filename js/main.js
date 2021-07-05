e38003d05660307a4ad5dd541e104fb7075c31a6
Estefani <stef45689@gmail.com>
2021-07-05 11:58:09 -0500

    More

--- js/main.js
+++ js/main.js
@@ -330,3 +330,5 @@ return count
 //Replave Every nth
 function replaceNth(text, n, oldValue, newValue) {}
 let i = 0
+return n > 0 ? text.replace(new RegExp(oldValue, "g"), c => ++i % n === 0 ? newValue : c) : text
+}
