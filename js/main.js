2a74e3f8e32296bda4f4bd13c8fbcfc90d848f15
Estefani <stef45689@gmail.com>
2021-07-06 08:54:02 -0500

    More

--- js/main.js
+++ js/main.js
@@ -332,3 +332,5 @@ function replaceNth(text, n, oldValue, newValue) {}
 let i = 0
 return n > 0 ? text.replace(new RegExp(oldValue, "g"), c => ++i % n === 0 ? newValue : c) : text
 }
+
+//Convert String to Camel Case
