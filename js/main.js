2e79a705d0ece702d6e62d991154b6cc1f575418
Estefani <stef45689@gmail.com>
2021-08-09 10:46:22 -0500

    More

--- js/main.js
+++ js/main.js
@@ -367,3 +367,4 @@ function autocomplete(input, d){
   input=input.replace(/[^a-zA-Z]/gi,'')
   const arr=[];
   for (let i=0;i<d.length;i++){
+    if (d[i].slice(0,input.length).toLowerCase()===input.toLowerCase()){ arr.push(d[i]) }
