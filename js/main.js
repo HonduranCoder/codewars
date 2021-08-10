b1eba12d89eaff06ac13d5d18c7f452c7d7eed99
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2021-08-10 11:03:14 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -369,3 +369,5 @@ function autocomplete(input, d){
   for (let i=0;i<d.length;i++){
     if (d[i].slice(0,input.length).toLowerCase()===input.toLowerCase()){ arr.push(d[i]) }
   }
+  return arr.slice(0,5)
+}
