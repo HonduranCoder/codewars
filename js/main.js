62ae9b9282cbc812e42b7e7218c6a4b64d4e17c2
Estefani <stef45689@gmail.com>
2021-09-07 10:12:05 -0500

    More

--- js/main.js
+++ js/main.js
@@ -389,3 +389,7 @@ function clean_string(s) {
 //A String of Sorts
 function sortString(string,ordering) {
   ordering=ordering.split``
+  let arr1 = string.split``.filter(v=>ordering.includes(v)).sort((a,b)=>ordering.indexOf(a)-ordering.indexOf(b))
+  let arr2 = string.split``.filter(v=>!ordering.includes(v))
+  return arr1.join``+arr2.join``
+}
